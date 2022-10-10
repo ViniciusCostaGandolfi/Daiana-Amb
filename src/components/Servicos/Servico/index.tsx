80516b20cfbc2b8e10c41409fc0ff4fc9dcc631c
Vinicius Costa Gandolfi <81584546+ViniciusCostaGandolfi@users.noreply.github.com>
2022-10-10 10:55:16 -0300

    Update index.tsx

--- src/components/Servicos/Servico/index.tsx
+++ src/components/Servicos/Servico/index.tsx
@@ -34,7 +34,7 @@ function Servico(props: Props) {
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true }}
-            transition={{ duration: 1 }}
+            transition={{ duration: 0.5 }}
             variants={{
             visible: { opacity: 1, x: 0 },
             hidden: { opacity: 0, x: -150 }

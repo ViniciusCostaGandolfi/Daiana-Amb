82c0f6ba1de99a7048265ed4e777b6c851e10a8f
Vinicius Costa Gandolfi <81584546+ViniciusCostaGandolfi@users.noreply.github.com>
2023-01-24 16:10:26 -0300

    Update index.tsx

--- src/components/SobreNos/index.tsx
+++ src/components/SobreNos/index.tsx
@@ -13,7 +13,7 @@ function SobreNos() {
     return (
         <section className={styles['sobreNos']}>
             <h2 className={styles['sobreNos__titulo']}>Sobre Nós</h2>
-            <p className={styles['sobreNos__descricao']}>Nós da Daiana Silva Consultoria e Gestão Ambiental entendemos que cada um de nossos clientes tem suas demandas e especificidades, que devem ser analizadas individualmente, utilizando toda experiencia e conhecimento técnico de quem atua na área há mais de uma década.</p>
+            <p className={styles['sobreNos__descricao']}>Nós da Daiana Silva Consultoria e Gestão Ambiental entendemos que cada um de nossos clientes tem suas demandas e especificidades, que devem ser analizadas individualmente, utilizando toda experiência e conhecimento técnico de quem atua na área há mais de uma década.</p>
 
             <div className={styles['sobreNos__MVVs']}>
                 {lista.map((item) => (

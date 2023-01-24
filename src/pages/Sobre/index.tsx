6a1f94883e26423ab855d898de9246df2bf2dc93
Vinicius Costa Gandolfi <81584546+ViniciusCostaGandolfi@users.noreply.github.com>
2023-01-24 16:08:47 -0300

    Update index.tsx

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -23,7 +23,7 @@ export default function Sobre() {
                         <h2 className={styles['sobre__DNA__texto__titulo']}>Nosso DNA</h2>
                         <p className={styles['sobre__DNA__texto__descricao']}>A Daiana Silva Gestão Ambiental é uma empresa de consultoria ambiental localizada em Limeira/SP, que atende diversas cidades no Brasil .</p>
                         <p className={styles['sobre__DNA__texto__descricao']}> Nossa empresa soluciona problemas e possibilita a regularização de intervenções ambientais, atuamos de modo preventivo, auxiliando nossos clientes nas tomadas de decisão em seus projetos, reduzindo seus impactos ambientais e financeiros, além de ampliar as possibilidades ligadas a gestão ambiental.</p>
-                        <p className={styles['sobre__DNA__texto__descricao']}>Nosso atendimento aos clientes é proficional e feito na medida, considerando todas as pecularidades, prezamos por conhece-los e realizar o serviço do melhor modo possível. 
+                        <p className={styles['sobre__DNA__texto__descricao']}>Nosso atendimento aos clientes é profissional e feito na medida, considerando todas as peculiaridades, prezamos por conhece-los e realizar o serviço do melhor modo possível. 
                         Aqui na Daiana Silva Gestão Ambiental, levamos os desafios de nossos clientes, como nossos, sempre encontrando as melhores soluções para cada desafio..</p>
                     </div>
                     <div className={styles['sobre__DNA__imagem']}>

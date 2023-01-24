bb71a6e47dbcb0e1b5810a90c2ecc31b57678b05
Vinicius Costa Gandolfi <81584546+ViniciusCostaGandolfi@users.noreply.github.com>
2023-01-24 15:20:11 -0300

    Update index.tsx

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -20,9 +20,8 @@ export default function Contato(){
                 </h2>
                 <iframe 
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.5460640701854!2d-47.39829804834951!3d-22.558671131142777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c880936a3dcccf%3A0x97fca9996212fa51!2sR.%20Boa%20Vista%2C%20238%20-%20Boa%20Vista%2C%20Limeira%20-%20SP%2C%2013486-110!5e0!3m2!1spt-BR!2sbr!4v1665077720540!5m2!1spt-BR!2sbr" 
-                    loading="lazy" 
-                    referrerPolicy="no-referrer-when-downgrade"
                     className={styles['mapaContainer__mapa']}
+                    title="Mapa Daiana Silva Engenharia Ambiental"
                 >
                     
                 </iframe>
